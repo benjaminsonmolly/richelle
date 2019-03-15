625c878f00ae55b9b82fd2967b05dd255ec71adf
Molly Benjaminson <benjaminsonmolly@gmail.com>
2019-03-15 16:02:42 -0700

    Added styling.

--- background.js
+++ background.js
@@ -5,18 +5,18 @@ document.addEventListener('DOMContentLoaded', function() {
     'The heart of a shrimp is located in its head.', 'A snail can sleep for three years.', 'Slugs have four noses.'
   ];
   var questions = [
-    'Do ants feel love?', 'Do ants go to heaven?'
+    'Do ants feel love?', 'Do ants go to heaven?', 'What do wolves eat?', 'How long do wolves live?'
   ];
   if(factButton){
     factButton.addEventListener('click', function() {
       var random = facts[Math.floor(Math.random()*facts.length)];
-      alert(random);
+      document.getElementById("f_result").innerHTML = random;
     }, false);
   }
   if(questionButton){
     questionButton.addEventListener('click', function() {
       var randomQ = questions[Math.floor(Math.random()*questions.length)];
-      alert(randomQ);
+      document.getElementById("q_result").innerHTML = randomQ;
     }, false);
   }
 }, false);
